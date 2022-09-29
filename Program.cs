56b4669179af61541cc1df688efa23948fa46027
alim <7475149@mail.ru>
2022-09-29 23:04:04 +0600

    выполнен первый метод генерации массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача: написать программу которая из имеющегося массива создаст массив из элементов которые короче или равны трем символам
+
+string [] Generate_array()
+{ 
+    string [] array = new string [10];
+    for (int i=0; i<10; i++)
+    {
+        Console.WriteLine("Введите элемент массива");
+        array [i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+
+//---------------------------------основная программа------------------------------------
+string [] arr = Generate_array();
+
+
